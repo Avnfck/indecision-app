1e6d9fd4d6991af6409e9e8e272cfa8d64c0b8df
Arek <arek.let@gmail.com>
2023-02-18 11:00:54 +0100

    setting up react localy and webpack and babel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('asdasd asd asda')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'testing 123');
+
+ReactDOM.render(template, document.getElementById('app'));
